fd467f28692c3ff67f5eb9c808cf2879b6aa0110
Mehrzad ardeshiri <mehrzad.ardeshiri@gmail.com>
2025-11-30 12:04:41 +0330

    Refactor TypeScript configurations and update package structure. Change type package name to @monorepo/types, adjust paths in tsconfig.json for backend and frontend, and add Filters module with Mongoose integration.

---
+++ apps/backend/src/filters/dto/filter.dto.ts
@@ -0,0 +1,77 @@
+import { FilterActions } from '../../../../../packages/types/enums/FilterActions';
+import {
+  IsBoolean,
+  IsEnum,
+  IsOptional,
+  IsString,
+  ValidateIf,
+} from 'class-validator';
+
+export class CreateFilterDto {
+  @IsString()
+  name!: string;
+  @IsString()
+  pattern!: string;
+  @IsEnum(FilterActions)
+  action!: FilterActions;
+  @IsString()
+  @IsOptional()
+  @ValidateIf(
+    (o) =>
+      o.action === FilterActions.REMOVE_WORD ||
+      o.action === FilterActions.REMOVE_LINE ||
+      o.action === FilterActions.REGEX_REPLACE,
+  )
+  replacement?: string;
+  @IsBoolean()
+  @IsOptional()
+  isRegex?: boolean;
+
+  @IsBoolean()
+  @IsOptional()
+  isActive?: boolean;
+}
+
+export class UpdateFilterDto {
+  @IsString()
+  @IsOptional()
+  name?: string;
+
+  @IsString()
+  @IsOptional()
+  pattern?: string;
+
+  @IsEnum(FilterActions)
+  @IsOptional()
+  action?: FilterActions;
+
+  @IsString()
+  @IsOptional()
+  replacement?: string;
+
+  @IsBoolean()
+  @IsOptional()
+  isRegex?: boolean;
+
+  @IsBoolean()
+  @IsOptional()
+  isActive?: boolean;
+}
+export class TestFilterDto {
+  @IsString()
+  text!: string;
+
+  @IsString()
+  pattern!: string;
+
+  @IsEnum(FilterActions)
+  action!: FilterActions;
+
+  @IsBoolean()
+  @IsOptional()
+  isRegex?: boolean;
+
+  @IsString()
+  @IsOptional()
+  replacement?: string;
+}
--- apps/backend/src/queue/message-processor.service.ts
+++ apps/backend/src/queue/message-processor.service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { FiltersService } from 'src/filters/filters.service';
 
 @Injectable()
 export class MessageProcessorService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly logger = new Logger(MessageProcessorService.name);
+  private readonly urlRegex = /https?:\/\/[^\s]+/g;
+  private readonly mentionRegex = /@[a-zA-Z0-9_]+/g;
+  constructor(private readonly filtersService: FiltersService) {}
+
+  async processMessage(text: string): Promise<string> {
+    let processedText = text;
+    //get active filters
+    const filters=await this.filtersService.getActiveFilters()
+    for (const filter of filters){
+      processedText=await
+    }
   }
 }
